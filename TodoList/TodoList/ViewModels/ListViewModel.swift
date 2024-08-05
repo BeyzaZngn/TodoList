f7223f6ace86fe330960619854d830d5225e84d5
BeyzaZngn <beyzazengin76@gmail.com>
2024-08-05 12:31:19 +0300

    saveItems() added and getItems() updated.

--- TodoList/TodoList/ViewModels/ListViewModel.swift
+++ TodoList/TodoList/ViewModels/ListViewModel.swift
@@ -21,22 +21,24 @@ import Foundation
 
 class ListViewModel: ObservableObject {
     
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    
+    let itemsKey: String = "items_list"
     
     init() {
-        
+        getItems()
     }
     
     func getItems() {
         
-        let newItems = [
-            
-            ItemModel(title: "This is the first title!", isCompleted: false),
-            ItemModel(title: "This is the second!", isCompleted: true),
-            ItemModel(title: "Third!", isCompleted: false)
-            
-        ]
-        items.append(contentsOf: newItems)
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder ().decode([ItemModel].self, from: data)
+        else { return }
         
     }
     
@@ -59,6 +61,12 @@ class ListViewModel: ObservableObject {
             items[index] = item.updateCompletion()
         }
  
+    }
+    
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: "items_list")
+        }
     }
     
 }
