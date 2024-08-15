d32ff7dfb072e99adb682b1e026ac62c7c1a0757
BeyzaZngn <beyzazengin76@gmail.com>
2024-08-15 13:49:39 +0300

    NoItemsView Created.

---
+++ TodoList/TodoList/Views/NoItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  NoItemsView.swift
+//  TodoList
+//
+//  Created by Beyza Zengin on 15.08.2024.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoItemsView()
+}
